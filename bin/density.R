9479cb6001dacca273b7638a23791ec7d006b5aa
Jeff Furlong <jfurlong@Alexs-iMac-Jeff.local>
2023-11-29 12:00:10 -0800

    Updated density.R to change plot colors and set fixed plot aspect ratio.

--- bin/density.R
+++ bin/density.R
@@ -6,6 +6,7 @@ args = commandArgs(trailingOnly=TRUE)
 srr <- args[1]
 ref <- args[2]
 srr_ref <- paste(srr, ref, sep="_")
+
 ref_inserts = args[3]
 human_inserts = args[4]
 
@@ -15,21 +16,25 @@ data2 <- read.tabledata <- read.table(ref_inserts, header = F)
 length_cutoff <- 500
 d1 <- data1$V1[data1$V1 <= length_cutoff]
 d2 <- data2$V1[data2$V1 <= length_cutoff]
-df1 <- data.frame(PLOT_DATA='Human', V2=d1 )
-df1 <- df1[sample(nrow(df1), 100000), ]
-df2 <- data.frame(PLOT_DATA=srr_ref, V2=d2)
-df <- rbind(df1, df2)
 
+ref_legend <- paste(ref, paste("(", length(d2), ")", sep=""), sep=" ")
+human_legend <- paste("Human", paste("(", length(d1), ")", sep="")) #ifelse(length(d1)<=100000, paste("(", length(d1), ")", sep=""), "(100000)"), sep=" ")
+
+df1 <- data.frame(PLOT_DATA=human_legend, V2=d1 )
+df1 <- df1[sample(nrow(df1), 100000), ]
+df2 <- data.frame(PLOT_DATA=ref_legend, V2=d2)
+
+df <- rbind(df1, df2)
 colnames(df)[1] <- srr
 PLOT_COL <- sym(srr)
 
 # plot with median
 mu <- ddply(df, srr, summarise, grp.median=median(V2))
-p <- ggplot(df, aes(x=V2, fill=!!PLOT_COL))+geom_density(alpha=.25)+scale_fill_manual(values=c("red","blue"))+
+p <- ggplot(df, aes(x=V2, fill=!!PLOT_COL))+geom_density(alpha=.25)+scale_fill_manual(values=c("blue","red"))+
   scale_x_continuous(name = 'Fragment Size, bp',limits=c(50,500),breaks = seq(50,500,50),position ='bottom')+
   scale_y_continuous(name = 'Fraction of Reads',limits=c(0,0.035))+
   theme_bw()+theme(panel.grid=element_blank())+
-  geom_vline(data=mu, aes(xintercept=grp.median), color=c("red","blue"), linetype="dashed")
+  geom_vline(data=mu, aes(xintercept=grp.median), color=c("blue","red"), linetype="dashed") + theme(aspect.ratio=1)
 
 
 median_png <- paste(srr_ref, "dedup_inserts", "median.png", sep="_")
@@ -45,12 +50,12 @@ getmode <- function(v) {
 }
 mo1 <- getmode(d1)
 mo2 <- getmode(d2)
-p <- ggplot(df, aes(x=V2, fill=!!PLOT_COL))+geom_density(alpha=.25)+scale_fill_manual(values=c("red","blue"))+
+p <- ggplot(df, aes(x=V2, fill=!!PLOT_COL))+geom_density(alpha=.25)+scale_fill_manual(values=c("blue","red"))+
   scale_x_continuous(name = 'Fragment Size, bp',limits=c(50,500),breaks = seq(50,500,50),position ='bottom')+
   scale_y_continuous(name = 'Fraction of Reads',limits=c(0,0.035))+
   theme_bw()+theme(panel.grid=element_blank())+
-  geom_vline(aes(xintercept=mo1), color="red", linetype="dashed")+
-  geom_vline(aes(xintercept=mo2), color="blue", linetype="dashed")
+  geom_vline(aes(xintercept=mo1), color="blue", linetype="dashed")+
+  geom_vline(aes(xintercept=mo2), color="red", linetype="dashed") + theme(aspect.ratio=1)
 
 mode_png <- paste(srr_ref, "dedup_inserts", "mode.png", sep="_")
 ggsave(
